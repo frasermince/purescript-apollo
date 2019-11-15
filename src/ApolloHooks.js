c310735923995180971b90485098a0f1cc8e4221
Fraser Mince <frasermince@gmail.com>
2019-11-15 16:16:39 -0600

    Use thunk for mutation promise.

--- src/ApolloHooks.js
+++ src/ApolloHooks.js
@@ -3,7 +3,11 @@ var boost = require('apollo-boost');
 
 exports._gql = boost.gql;
 exports._useQuery = hooks.useQuery;
-exports._useMutation = hooks.useMutation;
+exports._useMutation = function(node, opts) {
+  return function() {
+    hooks.useMutation(node, opts);
+  }
+}
 exports.runThisFn1 = function(key){
   return function(self){
     return function(a){
